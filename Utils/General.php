f76d7f00d5d3f59e4e9c5e8f6a1bb66e5c2b203f
Peter Nassef <peter.nassef@gmail>
2020-06-26 14:26:09 +0200

    Add Function to generate random string

--- Utils/General.php
+++ Utils/General.php
@@ -249,5 +249,13 @@ class General {
         }
         return implode('_', $ret);
     }
-
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
