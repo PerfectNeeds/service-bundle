c15b774656c484c8c04bd44e2ed9702c7efa87ad
Peter Nassef <peter.nassef@gmail.com>
2022-01-11 17:49:21 +0200

    Increas creator and modifiedby length

--- Model/DateTimeTrait.php
+++ Model/DateTimeTrait.php
@@ -10,7 +10,7 @@ trait DateTimeTrait {
     protected $created;
 
     /**
-     * @ORM\Column(name="creator", type="string", length=30)
+     * @ORM\Column(name="creator", type="string", length=255)
      */
     protected $creator;
 
@@ -20,94 +20,47 @@ trait DateTimeTrait {
     protected $modified;
 
     /**
-     * @ORM\Column(name="modified_by", type="string", length=30)
+     * @ORM\Column(name="modified_by", type="string", length=255)
      */
     protected $modifiedBy;
 
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return DeliveryNote
-     */
+
     public function setCreated($created) {
         $this->created = $created;
 
         return $this;
     }
 
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
     public function getCreated() {
         return $this->created;
     }
 
-    /**
-     * Set creator
-     *
-     * @param string $creator
-     *
-     * @return DeliveryNote
-     */
     public function setCreator($creator) {
         $this->creator = $creator;
 
         return $this;
     }
 
-    /**
-     * Get creator
-     *
-     * @return string
-     */
     public function getCreator() {
         return $this->creator;
     }
 
-    /**
-     * Set modified
-     *
-     * @param \DateTime $modified
-     *
-     * @return DeliveryNote
-     */
     public function setModified($modified) {
         $this->modified = $modified;
 
         return $this;
     }
 
-    /**
-     * Get modified
-     *
-     * @return \DateTime
-     */
     public function getModified() {
         return $this->modified;
     }
 
-    /**
-     * Set modifiedBy
-     *
-     * @param string $modifiedBy
-     *
-     * @return DeliveryNote
-     */
     public function setModifiedBy($modifiedBy) {
         $this->modifiedBy = $modifiedBy;
 
         return $this;
     }
-
-    /**
-     * Get modifiedBy
-     *
-     * @return string
-     */
+    
     public function getModifiedBy() {
         return $this->modifiedBy;
     }
