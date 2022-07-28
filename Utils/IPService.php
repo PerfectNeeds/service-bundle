c16e80d188cdf41e939b751c705a15ceedb9c9c1
Peter Nassef <peter.nassef@gmail.com>
2022-07-28 17:58:36 +0200

    Enhance getIP function

--- Utils/IPService.php
+++ Utils/IPService.php
@@ -2,9 +2,11 @@
 
 namespace PN\ServiceBundle\Utils;
 
-class IPService {
+class IPService
+{
 
-    public static function getIp() {
+    public static function getIp()
+    {
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             $ip = $_SERVER['HTTP_CLIENT_IP'];
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -12,13 +14,20 @@ class IPService {
         } else {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
+
+        if (strpos($ip, ",") !== false) {
+            return trim(explode(",", $ip)[0]);
+        }
+
         return $ip;
     }
 
-    public static function getIPLocation($ip = NULL) {
-        if ($ip == NULL) {
+    public static function getIPLocation($ip = null)
+    {
+        if ($ip == null) {
             $ip = self::getIp();
         }
+
         return json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
     }
 
