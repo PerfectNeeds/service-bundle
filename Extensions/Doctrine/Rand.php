aedd2b97088fd61fcacb82f8dd1e70d7dc9a3abe
Peter Nassef <peter.nassef@gmail.com>
2023-06-19 11:56:34 +0300

    Enhance Rand Doctrine Function

--- Extensions/Doctrine/Rand.php
+++ Extensions/Doctrine/Rand.php
@@ -2,21 +2,39 @@
 
 namespace PN\ServiceBundle\Extensions\Doctrine;
 
-use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\SimpleArithmeticExpression;
+use Doctrine\ORM\Query\Lexer;
 
 /**
  * RandFunction ::= "RAND" "(" ")"
  */
-class Rand extends FunctionNode {
+class Rand extends FunctionNode
+{
+    /**
+     * @var SimpleArithmeticExpression
+     */
+    private $expression = null;
 
-    public function parse(\Doctrine\ORM\Query\Parser $parser) {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        if ($this->expression) {
+            return 'RAND(' . $this->expression->dispatch($sqlWalker) . ')';
+        }
+
+        return 'RAND()';
     }
 
-    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker) {
-        return 'RAND()';
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $lexer = $parser->getLexer();
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        if (Lexer::T_CLOSE_PARENTHESIS !== $lexer->lookahead['type']) {
+            $this->expression = $parser->SimpleArithmeticExpression();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 }
